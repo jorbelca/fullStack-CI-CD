2803aab18ed7a24ac50e9a5d067c4645c366f189
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-03-17 20:28:36 +0100

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 
-const URL = process.env.MONGO_URI || MONGO.URI
+const URL = process.env.MONGO_URI || MONGOURI
 const PORT = process.env.PORT || 3001 
 
 module.exports = {
