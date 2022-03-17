0fd6291fafa5c8679612875f8403a3d05b7e91e9
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-03-17 19:48:15 +0100

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
-const URL = {mode === "test" ? secrets.MONGO_URI : process.env.MONGO_URI}
-const PORT = {mode === "test" ? secrets.PORT :process.env.PORT}
+ if (mode === "test"){const URL = secrets.MONGO_URI} else {const URL = process.env.MONGO_URI}
+ if (mode === "test"){ const PORT= secrets.PORT}{const PORT = process.env.PORT}
 
 module.exports = {
   URL,
